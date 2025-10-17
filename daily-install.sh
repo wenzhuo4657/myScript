653d36ae4a057415019a8c0305e4debe7bd98428
wenzhuo4657 <14783149521@163.com>
2025-10-17 17:41:35 +0800

    update

--- daily-install.sh
+++ daily-install.sh
@@ -106,7 +106,7 @@ server {
         alias /var/www/daily/;
     }
 
-    location /api/md {
+    location /api {
         proxy_pass http://127.0.0.1:8080;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
